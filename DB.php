6ba5e03b313221ac9b59fa6ec8cbbd5871e7e2c7
GustafJ <gustaf89@hotmail.se>
2021-05-05 11:57:48 +0200

    erase obsolete comments

--- DB.php
+++ DB.php
@@ -10,14 +10,8 @@ class DB
 
   public function __construct()
   {
-    try {
-      $this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->db,  $this->username, $this->password);
-      // set the PDO error mode to exception
-      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "Connected successfully";
-    } catch (PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
-    }
+    $this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->db,  $this->username, $this->password);
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
 
   public function get_all_contacts()
